8b23d0185d2d8362ed4435461f0e0c04a947cc15
unknown <elidejuma@gmail.com>
2019-11-20 14:52:56 +0300

    Chore postgress setup

--- config/connection.js
+++ config/connection.js
@@ -7,7 +7,9 @@ exports.pool = new Pool({
   host: process.env.POSTGRES_HOST,
   database: process.env.POSTGRES_DATABASE,
   password: process.env.POSTGRES_PASSWORD,
-  port: process.env.POSTGRES_PORT
+  port: process.env.POSTGRES_PORT,
+  ssl: true,
+  // sslfactory: org.postgresql.ssl.NonValidatingFactory
 });
 
 
